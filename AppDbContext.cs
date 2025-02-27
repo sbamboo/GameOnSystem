fd3142fc26b97a3d446bba8e42c70694c1709cdf
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2025-02-27 08:54:50 +0100

    Update AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -7,10 +7,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GameOnSystem {
     internal class AppDbContext : DbContext {
+        private readonly bool _useSqlite;
+        private readonly string _mysqlConnectionString;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            if (_useSqlite) {
+                optionsBuilder.UseSqlite("Data Source=gameon_v2.db");
+            } else {
+                optionsBuilder.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
+            }
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseSqlite("Data Source=gameon_v2.db");
         }
-        public AppDbContext() {
+
+        public AppDbContext(bool useSqlite, string mysqlConnectionString) {
+            _useSqlite = useSqlite;
+            _mysqlConnectionString = mysqlConnectionString;
+
             Database.EnsureCreated();
         }
     }
